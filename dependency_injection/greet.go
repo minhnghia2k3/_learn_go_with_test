ab84532a179e72f67b6dcb0aa227a8d93bcf5c6d
Nghia Le <brozennvn@gmail.com>
2026-08-21 23:20:00 +0700

    Add DI chapters

---
+++ dependency_injection/greet.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func GreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "Chris")
+}
+
+func main() {
+	// Greet(os.Stdout, "Chris")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(GreetHandler)))
+}
